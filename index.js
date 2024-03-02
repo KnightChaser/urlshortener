143eff1b11a29410bc1c6243eecfbf572ce3d1a2
Lee Garam <agerio100@naver.com>
2024-03-03 00:56:34 +0900

    Built a basic frontend

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 // A simple hello world express.js app
 express = require("express");
 dotenv = require("dotenv");
+path = require("path");
 dotenv.config();
 
 const app = express();
 const port = process.env.SERVER_ACCESS_PORT || 7777;
 
 app.use(express.json());
+app.use("/", express.static(path.resolve(__dirname, "views")))
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+app.get("/", (request, response) => {
+    response.send("Hello World");
 })
---
+++ views/index.js
@@ -0,0 +1,12 @@
+function shortenUrl() {
+    // Get the original URL from the input field
+    var originalUrl = document.getElementById('originalUrl').value;
+
+    // TODO: Perform the URL shortening process (you may need a backend for this)
+
+    // Display the result on the page
+    // var resultContainer = document.getElementById('result');
+    // var shortenedUrlInput = document.getElementById('shortenedUrl');
+    // shortenedUrlInput.value = shortenedUrl;
+    // resultContainer.classList.remove('hidden');
+}
